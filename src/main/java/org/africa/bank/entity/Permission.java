d51ce809776cafe172e4fc0362ca0e2f801cb459
mor mbathie <mormbathie98@gmail.com>
2026-05-09 21:52:29 +0100

    Ajout des entite Role et Permission

---
+++ src/main/java/org/africa/bank/entity/Permission.java
@@ -0,0 +1,17 @@
+package org.africa.bank.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.africa.bank.constants.PermissionName;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Permission {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(unique = true, nullable = false)
+    private PermissionName name;
+}
